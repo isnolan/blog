029c5378beec8ef83ff1f28ba930b255cb454a34
yhostc <yhostc@gmail.com>
2024-02-24 19:42:40 +0800

    build: update mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,8 +2,9 @@ import type { MDXComponents } from 'mdx/types'
 import Image, { type ImageProps } from 'next/image'
 
 export function useMDXComponents(components: MDXComponents) {
+  
   return {
     ...components,
-    Image: (props: ImageProps) => <Image {...props} />,
+    Image: (props: ImageProps) => <Image {...props}  />,
   }
 }
